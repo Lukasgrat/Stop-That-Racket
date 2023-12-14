86b4fc36433ae2a28ed6b00b8f3956300e584efa
LukTeg <tegge.l@northeastern.edu>
2023-12-13 21:51:45 -0500

    Updated pieces to work on board spaces and added multiple pieces with IDs

---
+++ Assets/gameRoom.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameRoom : MonoBehaviour
+{
+    public GameObject[] neighborRooms;
+    public Vector3[] characterPositions;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/pieceTest.cs
+++ Assets/pieceTest.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+using System.Linq;
 public class pieceTest : MonoBehaviour
 {
     public Sprite regularSprite;
+    public int charcter_ID;
     public Sprite highlightedSprite;
     public GameObject LevelManager;
     private bool isSelected;
+    public GameObject initialRoom;
     Camera m_Camera;
+    [System.NonSerialized]
+    public GameObject currentRoom;
     // Start is called before the first frame update
     void Start()
     {
         m_Camera = Camera.main;
         isSelected = false;
+        currentRoom = initialRoom;
     }
 
     // Update is called once per frame
@@ -22,22 +27,37 @@ public class pieceTest : MonoBehaviour
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.GetRayIntersection(ray, Mathf.Infinity);
             var spriteRender = GetComponent<SpriteRenderer>();
-            if (hit.collider != null && hit.collider.name == name)
+            bool wasHit = false;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D[] hits = Physics2D.GetRayIntersectionAll(ray, Mathf.Infinity);
+            foreach (var hit in hits)
             {
-                Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
-                spriteRender.sprite = highlightedSprite;
-                isSelected = true;
-            }
-            else
-            {
-                if (isSelected)
+                if (hit.collider.name == name)
                 {
-                    var movePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-                    var nextPosition = new Vector3(movePosition.x,movePosition.y,0);
-                    movePlayer(nextPosition);
+                    Debug.Log("Target Position: " + hit.collider.gameObject.transform.position);
+                    spriteRender.sprite = highlightedSprite;
+                    isSelected = true;
+                    wasHit = true;
+                }
+            }
+            if(! wasHit)
+            {   
+                if(hits.Length > 0) { 
+                    int index = roomIndex(currentRoom.GetComponent<gameRoom>().neighborRooms, hits[0]);
+                    if (isSelected && index != -1)
+                    {
+                        currentRoom = currentRoom.GetComponent<gameRoom>().neighborRooms[index];
+                        if (charcter_ID < currentRoom.GetComponent<gameRoom>().characterPositions.Length)
+                        {
+                            movePlayer(currentRoom.transform.position + (currentRoom.GetComponent<gameRoom>().characterPositions)[charcter_ID]);
+                        }
+                        else
+                        {
+                            movePlayer(currentRoom.transform.position);
+                        }
+
+                    }
                 }
                 spriteRender.sprite = regularSprite;
                 isSelected = false;
@@ -50,5 +70,17 @@ public class pieceTest : MonoBehaviour
         {
             transform.position = pos;
         }
+    }
+   // returns the index of the neigboring room the ray is hitting. If its not a neighboring room, returns -1
+    int roomIndex(GameObject[] rooms, RaycastHit2D hit)
+    {
+        for (int x = 0; x < rooms.Length; x++)
+        {
+            if (rooms[x].name == hit.collider.name) 
+            {
+                return x;
+            }
+        }
+        return -1;
     }
 }
